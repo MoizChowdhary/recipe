0a8fd12d4ca5da1476db1aeafcb5bd52d1ad6155
MoizChowdhary <moizchowdhary090@gmail.com>
2025-02-23 14:47:44 +0530

    adding all files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-     <img src="/Biryani.webp" class="img-fluid w-100 pt-1" alt="..."/ >
+     <img src="/reciepebook.webp" class="img-fluid w-100 pt-1" alt="..."/ >
      <div class="row row-cols-1 row-cols-md-2 g-4 m-2">
   <div class="col">
     <div class="card">
-      <img src="/Biryani.webp" class="card-img-top" alt="..."/>
+      <img src="/th.jpg" class="card-img-top" alt="..."/>
       <div class="card-body">
         <h5 class="card-title">Chicken Biryani</h5>
-        <p class="card-text"> A flavorful rice dish cooked with spices, meat (chicken, mutton, or fish), and aromatic herbs.</p>
+        <p class="card-text"> Basmati rice layered with tender chicken pieces, slow-cooked with aromatic spices like cardamom, cinnamon, and saffron, creating a flavorful and hearty dish.</p>
       </div>
     </div>
   </div>
@@ -18,26 +18,26 @@ const Home = () => {
     <div class="card">
       <img src="/angara.jpg" class="card-img-top" alt="..."/ >
       <div class="card-body">
-        <h5 class="card-title">Angara</h5>
+        <h5 class="card-title">Chicken Angara</h5>
         <p class="card-text">A spicy, smoky grilled chicken dish with bold masala flavors.</p>
       </div>
     </div>
   </div>
   <div class="col">
     <div class="card">
-      <img src="/gulab.png" class="card-img-top" alt="..."/ >
+      <img src="/Chole puri.jpg" class="card-img-top" alt="..."/ >
       <div class="card-body">
-        <h5 class="card-title">Gulab jamun</h5>
-        <p class="card-text">Soft, deep-fried milk-based dumplings soaked in sweet rose-flavored syrup.</p>
+        <h5 class="card-title">Chole Puri</h5>
+        <p class="card-text">Chickpeas simmered in a flavorful masala, and Puri is a type of deep-fried, puffed bread made from whole wheat flour. </p>
       </div>
     </div>
   </div>
   <div class="col">
     <div class="card">
-      <img src="/ice cream1.jpg" class="card-img-top" alt="..." />
+      <img src="/Pav-Bhaji.jpg" class="card-img-top" alt="..." />
       <div class="card-body">
-        <h5 class="card-title">Ice Cream</h5>
-        <p class="card-text"> A frozen dessert made from dairy, sugar, and flavors like vanilla, chocolate, or fruit.</p>
+        <h5 class="card-title">Pav-Bhaji</h5>
+        <p class="card-text"> A spiced mixture of mashed vegetables in a thick gravy served with bread</p>
       </div>
     </div>
   </div>
